fd6331b83e678b674b67d069ecaa1b796fff6346
Romain Barissat <romain@barissat.com>
2015-10-09 14:57:28 +0200

    fixing heroku compatibility

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Hapi            = require('hapi'),
 var server = new Hapi.Server();
 
 // Define PORT number
-server.connection({host: 'localhost', port: (process.env.PORT || 7002)});
+server.connection({host: '0.0.0.0', port: (~~process.env.PORT || 3000)});
 
 // Define Swagger options
 var swaggerOptions = {
